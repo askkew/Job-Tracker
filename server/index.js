7fa80a790be46ae5666c645c2fea6acc5e694da2
Lucas Overbey <lucasaoverbey@gmail.com>
2023-03-14 14:59:38 -0500

    finished backend

--- server/index.js
+++ server/index.js
@@ -15,8 +15,41 @@ mongoose.connect(uri);
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('Connected to MongoDB sucessfully');
-})
+});
+
+// Define schema and model
+const Schema = mongoose.Schema;
+const exampleSchema = new Schema({
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  email: { type: String, required: true },
+});
+
+const Example = mongoose.model('Example', exampleSchema);
+
+// Add document
+app.post('/add', (req, res) => {
+  const { name, age, email } = req.body;
+  const example = new Example({ name, age, email });
+  example.save()
+    .then(() => res.status(200).json('Example added'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Update document
+app.put('/update/:id', (req, res) => {
+  Example.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then(() => res.status(200).json('Example updated'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Remove document
+app.delete('/remove/:id', (req, res) => {
+  Example.findByIdAndRemove(req.params.id)
+    .then(() => res.status(200).json('Example removed'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
-})
+});

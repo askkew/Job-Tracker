8dbbb8fc8955de612186f8b334e36038f5279aa9
Lucas Overbey <lucasaoverbey@gmail.com>
2023-03-15 17:11:33 -0500

    added repo link button

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -3,6 +3,7 @@ import "./index.css";
 import { FC } from "react";
 import { CustomButton } from "../../utils/button";
 import { FiGithub } from "react-icons/fi";
+import { Link } from "@mui/material";
 
 export interface NavBarTypes {
   theme: any,
@@ -19,7 +20,11 @@ const NavBar: FC<NavBarTypes> = ({ handleToggleTheme }) => {
         </label>
       </div>
       {/* <h1>Job Tracker</h1> */}
-      <CustomButton style={{width: '180px', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>Github Repo <FiGithub style={{marginLeft: '6px'}}/></CustomButton>
+      <CustomButton style={{width: '180px', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+        <Link style={{color: 'white', textDecoration: 'none'}} href="https://github.com/askkew/Job-Tracker">
+          Github Repo <FiGithub style={{marginLeft: '6px'}}/>
+        </Link>
+      </CustomButton>
     </NavBarContainer>
   )
 }
